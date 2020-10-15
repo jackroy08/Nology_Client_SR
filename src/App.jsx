9fab5ed995c1cc68a351e72818f9c7f3dd99ea46
John Guilding <jwguilding@gmail.com>
2020-10-15 15:25:41 +0100

    set up done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Operator from './Pages/Operator'
+
+const App = () => {
+  return (
+    <Operator />
+  );
+}
+
+export default App;
---
+++ src/Pages/Operator/Operator.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Operator = () => {
+
+    
+    return (
+        <section>
+            <button>Start shift</button>
+            <button>Accept Vehicle</button>
+            <button>Report a Problem</button>
+            <button>Submit Load</button>
+        </section>
+    )
+}
+
+export default Operator;
