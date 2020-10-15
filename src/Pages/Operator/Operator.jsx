8dc4a5ac19e2ba4c5a250f82b308b22a376bdb96
matthewjchurch <matthewjchurch@gmail.com>
2020-10-15 19:45:48 +0100

    quote formatting

--- src/Pages/Operator/Operator.jsx
+++ src/Pages/Operator/Operator.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Link } from '@reach/router';
+import { Link } from "@reach/router";
 import styles from "./Operator.module.scss";
 
 const Operator = () => {
