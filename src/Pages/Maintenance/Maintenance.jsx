a55f6da2a0b597beb4cece1ab1cf5e2333a7be26
matthewjchurch <matthewjchurch@gmail.com>
2020-10-16 17:12:30 +0100

    witchcraft

---
+++ src/Pages/Maintenance/Maintenance.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MaintenanceList from "../../Components/MaintenanceList";
+import MaintenanceAside from "../../Components/MaintenanceAside";
+
+const Maintenance = () => {
+    return (
+        <>
+            <MaintenanceList />
+            <MaintenanceAside />   
+        </>
+    )
+}
+export default Maintenance;
