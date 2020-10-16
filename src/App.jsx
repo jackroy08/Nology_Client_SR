29fcef7700eda2252c84f014d7472099de14e95c
b-tennant <70852507+b-tennant@users.noreply.github.com>
2020-10-16 16:59:45 +0100

    Added in dummy data and began dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import Login from "./pages/Login";
+import Styles from "./App.module.scss";
 import Maintenance from "./pages/Maintenance";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className={Styles.App}>
       <Router>
         <Maintenance path="/Maintenance" />
       </Router>
--- src/pages/Maintenance/MaintenanceDropdown/MaintenanceDropdown.jsx
+++ src/pages/Maintenance/MaintenanceDropdown/MaintenanceDropdown.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import Styles from "./MaintenanceDropdown.module.scss";
 
 const MaintenanceDropdown = () => {
-    return <p>I am a dropdown</p>
+    return <div className={Styles.dropdownContainer}>
+        <p>Problem</p>
+        <p>Hazard Class</p>
+        <p>Time reported</p>
+        <p>Fix by </p>
+        <p>Suggested fix</p>
+
+    </div>
 }
 
 export default MaintenanceDropdown;
