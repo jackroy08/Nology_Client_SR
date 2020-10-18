0805b4f328f5edab1afbc028165e833b70d8096a
Nick Truby <truby@me.com>
2020-10-18 13:45:42 +0100

    Styled Checklist Pages

--- src/Pages/Checklist/ClassAChecks/ClassAChecks.jsx
+++ src/Pages/Checklist/ClassAChecks/ClassAChecks.jsx
@@ -5,12 +5,14 @@ import Styles from "../Checklist.module.scss";
 const ClassAChecks = (props) => {
     const checkboxArr = props.checklistData;
     const getCheckList = item => (
-        <label key={item} htmlFor={item}>{item}
+        <>
+            <img src="" alt="IMG"/>
+            <label key={item} htmlFor={item}>{item}</label>
             <input type="checkbox" id={item} name={item} value={item}/>
-        </label>
+        </>
     );
     return (
-        <section className={Styles.checklistSection}>
+        <main className={Styles.pageGrid}>
             <h1>Class A Checks</h1>
             <form id="class-a-checks"> 
                 {checkboxArr.map(getCheckList)}
@@ -18,13 +20,13 @@ const ClassAChecks = (props) => {
             <textarea name="class-a-comment" id="class-a-checks"></textarea>
             <section className={Styles.navigation}>
                 <Link to="/operator">
-                    <button className={Styles.navButtons}>Back</button> 
+                    <button className={Styles.btn}>Back</button> 
                 </Link>
                 <Link to="/classb">
-                    <button className={Styles.navButtons}>Next</button>
+                    <button className={Styles.btn}>Next</button>
                 </Link>
             </section>
-        </section>
+        </main>
     )
 }
 
--- src/Pages/Checklist/ClassBChecks/ClassBChecks.jsx
+++ src/Pages/Checklist/ClassBChecks/ClassBChecks.jsx
@@ -5,12 +5,14 @@ import Styles from "../Checklist.module.scss";
 const ClassBChecks = (props) => {
     const checkboxArr = props.checklistData
     const getCheckList = item => (
-        <label key={item} htmlFor={item}>{item}
+        <>
+            <img src="" alt="IMG"/>
+            <label key={item} htmlFor={item}>{item}</label>
             <input type="checkbox" id={item} name={item} value={item}/>
-        </label>
+        </>
     );
     return (
-        <section className={Styles.checklistSection}>
+        <main className={Styles.pageGrid}>
             <h1>Class B Checks</h1>
             <form id="class-b-checks"> 
                 {checkboxArr.map(getCheckList)}
@@ -18,13 +20,13 @@ const ClassBChecks = (props) => {
             <textarea name="class-b-comment" id="class-b-checks"></textarea>
             <section className={Styles.navigation}>
                 <Link to="/classa">
-                    <button className={Styles.navButtons}>Back</button> 
+                    <button className={Styles.btn}>Back</button> 
                 </Link>
                 <Link to="/classc">
-                    <button className={Styles.navButtons}>Next</button>
+                    <button className={Styles.btn}>Next</button>
                 </Link>
             </section>
-        </section>
+        </main>
     )
 }
 
