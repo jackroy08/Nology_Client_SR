368dcd2c51e0124a4d0f24a0cbbb7f89669fc526
unknown <ewbhynes@gmail.com>
2020-10-20 15:17:35 +0100

    added first basic plant equipment object as proof of concept

---
+++ src/data/plantequipment.js
@@ -0,0 +1,14 @@
+const vehicleData = [
+    {
+        plantID: "001",
+        plantType: "Car",
+        currentTeam: null,
+        currentUser: null,
+        goStatus: "go",
+        checkItems: ["TO BE ADDED"],
+        lastChecked: [],
+        checkedLog: [],
+    }
+];
+
+export default vehicleData;
--- src/pages/Maintenance/Maintenance.jsx
+++ src/pages/Maintenance/Maintenance.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import MaintenanceList from "./MaintenanceList";
 import MaintenanceAside from "./MaintenanceAside";
 import Styles from "./Maintenance.module.scss";
+import vehicleData from "../../data/plantequipment";
 
 
 const Maintenance = () => {
+
+    console.log(vehicleData);
+
     return (
         <main className={Styles.pageGrid}>
             <MaintenanceAside />   
