d72d07478dd06ebea1c28d659d11f1d8d63fd7e1
unknown <ewbhynes@gmail.com>
2020-10-20 16:32:31 +0100

    Finished first draft of data file

--- src/pages/Maintenance/Maintenance.jsx
+++ src/pages/Maintenance/Maintenance.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import MaintenanceList from "./MaintenanceList";
 import MaintenanceAside from "./MaintenanceAside";
 import Styles from "./Maintenance.module.scss";
-import vehicleData from "../../data/plantequipment";
-
 
 const Maintenance = () => {
-
-    console.log(vehicleData);
 
     return (
         <main className={Styles.pageGrid}>
