86571036689b19c50c939a90aa5acef2486c6f58
andynology <70571501+andynology@users.noreply.github.com>
2020-10-21 16:34:39 +0100

    firebase

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDm6Rx2DYI-E4GTx5IIVkg0zv0h0jQfcOM",
+  authDomain: "shift-reporter-539f4.firebaseapp.com",
+  databaseURL: "https://shift-reporter-539f4.firebaseio.com",
+  projectId: "shift-reporter-539f4",
+  storageBucket: "shift-reporter-539f4.appspot.com",
+  messagingSenderId: "727143863517",
+  appId: "1:727143863517:web:ec5d549bdac47dc203f734"
+};
