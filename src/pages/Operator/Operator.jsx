20bed7f94593230ca83684d4d2d827ccf11a2e15
andynology <70571501+andynology@users.noreply.github.com>
2020-10-23 15:27:23 +0100

    temp

--- src/pages/Operator/Operator.jsx
+++ src/pages/Operator/Operator.jsx
@@ -6,44 +6,20 @@ const Operator = () => {
 
     const [isShiftStart, setIsShiftStart] = useState(false);
     const changeStart = isShiftStart ? "End shift" : "Start shift";
-    const btnColor = isShiftStart ? Styles.end : Styles.start
-
-    const [isAcceptVehicleAvailable, setIsAcceptVehicleAvailable] = useState(false);
-    const [isReportAProblemAvailable, setisReportAProblemAvailable] = useState(false);
-    const [isSubmitLoadAvailable, setIsSubmitLoadAvailable] = useState(false);
-
-
-    //if disable is true, return hidden style, else return shown style
-    const disabled = (disable) => disable ? Styles.hidden : Styles.shown
-
     return (
-        <main className={Styles.pageFlex}>
-            {/* 1 */}
+        <main className={Styles.pageGrid}>
             <button
-                className={`${Styles.btn} ${Styles.btnLG} ${btnColor}`}
-                onClick={() => {
-                    setIsShiftStart(!isShiftStart); 
-                    setIsAcceptVehicleAvailable(!isAcceptVehicleAvailable);
-                }}>
+                className={`${Styles.btn} ${Styles.btnLG}`}
+                onClick={() => (setIsShiftStart(!isShiftStart))}>
                 {changeStart}
             </button>
-
-            {/* 2 */}
-            <Link to="/ClassA" className={disabled(!isAcceptVehicleAvailable)}>
-                <button className={`${Styles.btn} ${Styles.btnLG} ${Styles.btnPrimary}`}>Accept Vehicle</button>
+            <Link to="/ChecklistModal">
+                <button className={`${Styles.btn} ${Styles.btnLG}`}>Accept Vehicle</button>
             </Link>
-            <button className={`${Styles.btn} ${Styles.btnLG} ${Styles.btnPrimary} ${Styles.btnDisabled} ${disabled(isAcceptVehicleAvailable)}`}>Accept Vehicle</button>
-
-            {/* 3 */}
-            <Link to="/ClassA" className={disabled(!isReportAProblemAvailable)}>
-                <button className={`${Styles.btn} ${Styles.btnLG} ${Styles.btnPrimary}`}>Report a Problem</button>
+            <Link to="/ChecklistModal">
+                <button className={`${Styles.btn} ${Styles.btnLG}`}>Report a Problem</button>
             </Link>
-            <button className={`${Styles.btn} ${Styles.btnLG} ${Styles.btnPrimary} ${Styles.btnDisabled} ${disabled(isReportAProblemAvailable)}`}>Report a Problem</button>
-            
-            {/* 4 */}
-            <button className={`${Styles.btn} ${Styles.btnLG} ${Styles.btnPrimary} ${disabled(!isSubmitLoadAvailable)}`}>Submit Load</button>
-            <button className={`${Styles.btn} ${Styles.btnLG} ${Styles.btnPrimary} ${Styles.btnDisabled} ${disabled(isSubmitLoadAvailable)}`}>Submit Load</button>
-
+            <button className={`${Styles.btn} ${Styles.btnLG}`}>Submit Load</button>
         </main>
     )
 }
