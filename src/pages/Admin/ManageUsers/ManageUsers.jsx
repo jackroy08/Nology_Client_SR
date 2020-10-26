a0753cdfd8239f5d97e07849bddc5304843aec09
Nick Truby <truby@me.com>
2020-10-26 20:03:35 +0000

    moved user list component into ManagerUsers

--- src/pages/Admin/ManageUsers/ManageUsers.jsx
+++ src/pages/Admin/ManageUsers/ManageUsers.jsx
@@ -1,18 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './ManageUsers.module.scss';
-import UserList from "./UserList"
 import usersArr from '../../../data/users';
 import EditUser  from './EditUser/EditUser';
 import CreateUserForm from './CreateUserForm'
 import useModal from '../../../components/Modal/useModal';
 
 const ManageUsers = () => {
+    const {isShowing, toggle} = useModal();
+    const [filteredUsersArr, setFilteredUsersArr] = useState(usersArr);
+
+    const getUserItemJsx = (user) => {
+        return (
+            <li key={user.userID} className={Styles.userItem}>
+                <p>{user.fullNameStr}</p>
+                <p>{user.dateOfBirth}</p>
+                <p>{user.userID}</p>
+                <p>{user.isOnShift ? "Yes" : "No"}</p>
+                <p>{user.currentTeam ? user.currentTeam : "No team"}</p>
+                <button>Edit...</button>
+            </li>
+        )
+    };
+
+    const filterUsers = (userType) => {
+        if (userType) {
+        setFilteredUsersArr(usersArr.filter((user) => user.userType === userType))
+        } else {
+        setFilteredUsersArr(usersArr)
+        }
+    };
 
     return (
         <main className={Styles.pageGrid}>
             <CreateUserForm/>  
             {/* <EditUser user={usersArr[0]}/> */}
-            <UserList />    
+            <section className={Styles.userListSection}>
+                <header>
+                    <h3>Users</h3>
+                    <button onClick={toggle}>Create New User</button>
+                    <CreateUserForm isShowing={isShowing} hide={toggle} />
+                    <select onChange={(event) => filterUsers(event.target.value)}>
+                        <option value="">All Users</option>
+                        <option value="operator">Operator</option>
+                        <option value="supervisor">Supervisor</option>
+                        <option value="maintenance">Maintenance</option>
+                        <option value="management">Management</option>
+                    </select>
+                </header>                    
+                <ul className={Styles.userList}>
+                    <li className={Styles.columnTitles}>
+                        <h4>Full Name</h4>
+                        <h4>Date of Birth</h4>
+                        <h4>User ID</h4>
+                        <h4>On Shift?</h4>
+                        <h4>Team</h4>
+                        <h4>Action</h4>
+                    </li>
+                    {filteredUsersArr.map(getUserItemJsx)}
+                </ul>
+            </section>    
         </main>
     )
 }
