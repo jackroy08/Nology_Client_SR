236961414ac3f45c7c76d2e8d5913416767c4ff8
matthewjchurch <matthewjchurch@gmail.com>
2020-10-28 15:01:02 +0000

    added dummy service files

---
+++ src/services/SitesService.js
@@ -0,0 +1,19 @@
+// import Sites from "../data/users"
+
+const getSites  = () => {
+    console.log("get sites here")
+}
+
+const createSite  = () => {
+    console.log("create sites here")
+}
+
+const updateSite  = () => {
+    console.log("update sites here")
+}
+
+const deleteSite  = () => {
+    console.log("delete sites here")
+}
+
+export { getSites, createSite, updateSite, deleteSite };
