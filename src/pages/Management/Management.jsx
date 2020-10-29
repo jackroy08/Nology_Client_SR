d27a9186e6ba10415249b61d01c55d78d5e82693
unknown <ewbhynes@gmail.com>
2020-10-29 15:41:36 +0000

    added traffic light signal functions

--- src/pages/Management/Management.jsx
+++ src/pages/Management/Management.jsx
@@ -1,31 +1,57 @@
-import React from 'react'
-import {Router} from "@reach/router"
+import React, { useState, useEffect } from 'react'
 import Styles from './Management.module.scss';
 import { Link } from "@reach/router";
 import VehicleFeed from "./VehicleFeed";
 import TeamFeed from "./TeamFeed";
-import VehicleFeedReports from './VehicleFeed/VehicleFeedReports';
 
 const Management = () => {
 
-    return (
+    const [safetySignal, setSafetySignal] = useState();
+    const [redSignal, setRedSignal] = useState();
+    const [amberSignal, setAmberSignal] = useState();
+    const [greenSignal, setGreenSignal] = useState();
+
+    const safetySignalStatus = (status) => {
+        setSafetySignal(status);
+        if(status == "go") {
+            setGreenSignal(Styles.greenSignal)
+            setAmberSignal()
+            setRedSignal()
+
+        }  else if(status == "goBut") {
+            setRedSignal()
+            setAmberSignal(Styles.amberSignal)
+            setGreenSignal()
+
+        } else if (status == "noGo") {
+            setRedSignal(Styles.redSignal)
+            setAmberSignal()
+            setGreenSignal()
+        }
+    }
+
+    useEffect(() => {
+        safetySignalStatus("goBut");
+    })
+
+    return (   
         <>
-        <Router>
-            <main path="./" className={Styles.managementMain}>
+            <main className={Styles.managementMain}>
                 <section className={Styles.headingContainer}>
                     <h1 className={Styles.managementTitle}>Management</h1>
                     <Link to="../Admin"><button className={Styles.btn}>Go to admin page</button></Link>
                 </section>
                 {/* Green/red circle to signify site running/any errors */}
-                <div className={Styles.trafficLightSignal}></div>
+                <div className={Styles.trafficLightSignal}>
+                    <div className={`${Styles.defaultSignal} ${redSignal}`}></div>
+                    <div className={`${Styles.defaultSignal} ${amberSignal}`}></div>
+                    <div className={`${Styles.defaultSignal} ${greenSignal}`}></div>
+                </div>
                 <section className={Styles.liveFeedContainer}>
                     <VehicleFeed />
                     <TeamFeed />
                 </section>
             </main>
-            
-                <VehicleFeedReports path="VehicleFeedReports" />
-            </Router>
         </>
     )
 }
