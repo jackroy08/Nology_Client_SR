10a09445d3ddfd9e4c9fa72e884f10c4a706331a
Nick Truby <truby@me.com>
2020-11-13 13:06:56 +0000

    add a vehicle checklist styling complete

--- src/pages/Admin/ManageChecklists/ChecklistItem/ChecklistItem.jsx
+++ src/pages/Admin/ManageChecklists/ChecklistItem/ChecklistItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Styles from './ChecklistItem.module.scss';
 
 const ChecklistItem = () => {
     return (
-        <li className="checklistItem">
+        <li className={Styles.checklistItem}>
             <label htmlFor="class">Class of check:</label>
-            <select className="class" name="class">
+            <select className={Styles.selectPrimary} name="class">
                 <option value="classA">
                     classA
                 </option>
@@ -16,9 +17,9 @@ const ChecklistItem = () => {
                 </option>
             </select>
             <label htmlFor="check">Check:</label>
-            <input className="check" type="text"/>
+            <input className={Styles.inputPrimary} type="text"/>
             <label htmlFor="image">Icon URL address:</label>
-            <input className="image" type="text"/>
+            <input className={Styles.inputPrimary} type="text"/>
         </li>
     )
 }
